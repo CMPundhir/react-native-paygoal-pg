ec01af1e47d642b5c7782cf9666d56247c11bcc7
RupaMishra <rupamishraa2000@gmail.com>
2024-01-05 17:56:05 +0530

    npm package initialised and hosted over the npm rn-paygoal-pg@0.1.1

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,17 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
-}
+// export function multiply(a: number, b: number): Promise<number> {
+//   return Promise.resolve(a * b);
+// }
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export const index = (a: number, b: number) => {
+  return (
+    <View
+      style={{ backgroundColor: '#0f0f0f', width: '100%', paddingVertical: 24 }}
+    >
+      <Text>First number is :- {a}</Text>
+      <Text>Second number is :- {b}</Text>
+      <Text>Multiply of both number is :- {a * b}</Text>
+    </View>
+  );
+};
